d60f2ec93fadc52d383cef7f1d90ddcc63c8d4c3
harshi5555 <harshi5555@gmail.com>
2017-03-18 07:12:05 +0100

    changes

--- app/src/main/java/com/example/harshi/blooddonation/SignUp.java
+++ app/src/main/java/com/example/harshi/blooddonation/SignUp.java
@@ -1,18 +1,15 @@
 package com.example.harshi.blooddonation;
 
-import android.app.FragmentTransaction;
 import android.app.ProgressDialog;
-import android.content.Intent;
 import android.support.annotation.NonNull;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ProgressBar;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -21,25 +18,42 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
+public class SignUp extends AppCompatActivity {
+           /*Button buttonSignup;
+           EditText editTextName;
+           EditText editTextPassword1;
+           EditText textViewAge;
+           EditText editTextGender;
+           EditText editTextAddress;
+            EditText textView;
 
-public class Login extends AppCompatActivity implements View.OnClickListener {
-    Button buttonSignin;
-    EditText editTextEmail;
-    EditText editTextPassword;
-    TextView textViewSignup;
     ProgressDialog progressDialog;
-    FirebaseAuth firebaseAuth;
+           FirebaseAuth firebaseAuth;  */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-        initialize();
+        setContentView(R.layout.activity_sign_up);
+
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        Spinner spinner1 = (Spinner) findViewById(R.id.spinner1);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.bloodGroup_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+
+        ArrayAdapter<CharSequence> adapter1 = ArrayAdapter.createFromResource(this,
+                R.array.Gender_array, android.R.layout.simple_spinner_item);
+        adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner1.setAdapter(adapter1);
+
+
     }
 
-
-
-    private void initialize(){
-        buttonSignin = (Button)findViewById(R.id.login);
+    /*pri// Apply the adapter to the spinnervate void initialize(){
+        bspinner.setAdapter(adapter);uttonSignin = (Button)findViewById(R.id.login);
         editTextEmail =(EditText)findViewById(R.id.email);
         editTextPassword =(EditText)findViewById(R.id.password);
         textViewSignup = (TextView)findViewById(R.id.singup);
@@ -63,47 +77,53 @@ public class Login extends AppCompatActivity implements View.OnClickListener {
         }
         progressDialog.setMessage("Registering User...");
         progressDialog.show();
-     /*
         firebaseAuth.createUserWithEmailAndPassword(email,password)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if(task.isSuccessful()){
-                            Toast.makeText(Login.this,"Registered Successfully",Toast.LENGTH_LONG).show();
+                            Toast.makeText(SignUp.this,"Registered Successfully",Toast.LENGTH_LONG).show();
 
                         }else {
-                            Toast.makeText(Login.this,"Could not register. Please try again",Toast.LENGTH_LONG).show();
+                            Toast.makeText(SignUp.this,"Could not register. Please try again",Toast.LENGTH_LONG).show();
                         }
-                        progressDialog.hide();
+
                     }
                 });
-*/
 
-        firebaseAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-            @Override
-            public void onComplete(@NonNull Task<AuthResult> task) {
-                if(task.isSuccessful()){
-                    Toast.makeText(Login.this,"Login Successfully",Toast.LENGTH_LONG).show();
-                }else {
-                    Toast.makeText(Login.this,"Could not Login. Please check your credentials",Toast.LENGTH_LONG).show();
-                }
-                progressDialog.hide();
-            }
-        });
-        firebaseAuth.signOut();
+
     }
 
-    @Override
+
+      @Override
     public void onClick(View v) {
 
-        if(v == buttonSignin){
-            registerUser();
-        }
-        if(v==textViewSignup){
-            Intent intent = new Intent(Login.this,SignUp.class);
-            startActivity(intent);
-        }
+          if (v == buttonSignin) {
+              registerUser();
+          }
+          if (v == textViewSignup) {
 
+          }
+      } */
 
-    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
